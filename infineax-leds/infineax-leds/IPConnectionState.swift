0af09f7e1893cd8dc9c506e79c03c08d0df974ed
Joshua Kelley <jdkelley@samaritan.org>
2016-08-26 12:01:42 -0400

    Adds documentation for `IPConnectionState`.

--- infineax-leds/infineax-leds/IPConnectionState.swift
+++ infineax-leds/infineax-leds/IPConnectionState.swift
@@ -8,6 +8,25 @@
 
 import Foundation
 
+/**
+
+ Description: When the Infinite Peripherals SDK calls `connectionState`
+ on its delegate, it passes back a 0, 1, or 2. We attempt to instantiate a
+ `IPConnectionState` to verify that a valid value was returned.
+ 
+ An `IPConnectionState` instance also knows how to pretty print its value
+ by using the `toString` method.
+ 
+ ---
+
+ Type: Int32 Enum
+ 
+ 
+ Cases:
+ - Disconnected (rawValue: 0)
+ - Connecting   (rawValue: 1)
+ - Connected    (rawValue: 2)
+ */
 enum IPConnectionState: Int32 {
     
     case Disconnected = 0
