6b8f6220a15638612248fd9d8038faf1ed51b40b
Joshua Kelley <jdkelley@samaritan.org>
2016-08-26 11:50:06 -0400

    Adds base Xcode project.

---
+++ infineax-leds/infineax-leds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  infineax-leds
+//
+//  Created by Joshua Kelley on 8/26/16.
+//  Copyright © 2016 Joshua David Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
