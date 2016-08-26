2c2716739d1bd7c4c4addb17b0da08810bcba61f
Joshua Kelley <jdkelley@samaritan.org>
2016-08-26 12:59:49 -0400

    Adds bitmask building logic. Adds logging to textview on screen. Adds method to pass LED Mask to Infinea X.

--- infineax-leds/infineax-leds/ViewController.swift
+++ infineax-leds/infineax-leds/ViewController.swift
@@ -24,23 +24,49 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var loggingTextView: UITextView!
     
+    // MARK: Actions
+    
     @IBAction func switchToggled(sender: AnyObject) {
+        var mask: UInt32 = 0
+        if greenSwitch.on {
+            mask += 1
+        }
+        if redSwitch.on {
+            mask += 2
+        }
+        if blueSwitch.on {
+            mask += 4
+        }
+        log("Mask (\(mask) Sent: \(String(mask, radix: 2)))")
+        
+        passLEDMask(mask)
     }
+    
+    // MARK: View Controller Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         device.addDelegate(self)
         device.connect()
-        
+
     }
 
     private func log(text: String) {
         NSLog(text)
-        //textview.text.appendContentsOf("\r\n" + "\(NSDate()) > " + text)
+        loggingTextView.text.appendContentsOf("\r\n" + "\(NSDate()) > " + text)
     }
 }
 
 extension ViewController : DTDeviceDelegate {
+    
+    func passLEDMask(mask: UInt32) {
+        do {
+            try device.uiControlLEDsWithBitMask(mask)
+        } catch {
+            log("Could not set LED")
+        }
+    }
     
     // Gets called by DTDevices every time the connectionState changes.
     func connectionState(state: Int32) {
