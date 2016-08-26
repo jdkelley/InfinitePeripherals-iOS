b35360d93f7ff4d85d962c1a8ccd3910d31c7cbe
Joshua Kelley <jdkelley@samaritan.org>
2016-08-26 12:11:39 -0400

    Adds DTDevices bridging header and DTDevices.h.

---
+++ infineax-leds/infineax-leds/DTDevices-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  DTDevices-bridge.h
+//  SwiftDemo
+//
+//  Created by Flex on 8/31/15.
+//  Copyright (c) 2015 Datecs. All rights reserved.
+//
+
+#ifndef SwiftDemo_DTDevices_bridge_h
+#define SwiftDemo_DTDevices_bridge_h
+
+#import "DTdevices.h"
+#import <CommonCrypto/CommonDigest.h>
+#import <CommonCrypto/CommonCryptor.h>
+
+#endif
